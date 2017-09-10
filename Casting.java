c9d5796b43ad7b25b7d556344b85305df0140be2
Jesus Tanchanco <junot_t@yahoo.com>
2017-09-10 16:26:38 -0400

    first commit

---
+++ Casting.java
@@ -0,0 +1,18 @@
+class Casting{
+  public static void main(String[] args){
+    double d = 35.25;
+    double dd = 35.99;
+
+    int i = (int) d;
+
+    int ii = (int) dd;
+    System.out.println(i);
+    System.out.println(ii);
+  }
+}
+//   public static void main(String[] args){
+//     int i =35;
+//     float f = i;
+//     System.out.println("The number is: " + f);
+//   }
+// }
